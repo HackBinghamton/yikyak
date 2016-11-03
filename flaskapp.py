3b67060840af01a6f96ff1a96408dbe2db99c730
William Jagels <will.jagels@gmail.com>
2016-11-03 16:48:18 -0400

    Initial commit

---
+++ flaskapp.py
@@ -0,0 +1,42 @@
+import json
+from flask import Flask, request
+app = Flask(__name__)
+
+yaks = [
+        {'text': 'Hello world', 'votes': 5},
+        {'text': 'This is a yak', 'votes': -2}
+        ]
+
+
+@app.route("/", methods=['GET'])
+def mainpage():
+    return app.send_static_file('index.html')
+
+
+@app.route("/yak", methods=['POST'])
+def addyak():
+    return str(request.get_json())
+
+
+@app.route("/yaks", methods=['GET'])
+def getyaks():
+    return json.dumps(yaks)
+
+
+@app.route("/upvote", methods=['POST'])
+def upvote():
+    print(request.get_json())
+    yak = request.get_json()['id']
+    yaks[yak]['votes'] += 1
+    return "okay"
+
+
+@app.route("/downvote", methods=['POST'])
+def downvote():
+    yak = request.get_json()['id']
+    yaks[yak]['votes'] -= 1
+    return "okay"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
